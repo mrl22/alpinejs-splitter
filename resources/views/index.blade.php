005f2152f2c636c2efaeabfbaaae40abebb79e0d
Richard Leishman <richard@webfwd.co.uk>
2024-08-24 18:37:41 +0100

    Added Extra Context

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,8 +19,8 @@
         <div class="mt-10">
             <h1 class="text-2xl font-semibold mb-4">Known Issues</h1>
             <ul class="list-disc ms-5 mb-6">
-                <li>In split vertical, when dragging to the left, first pane view slides to the right.</li>
-                <li>In split horizontal, when dragging to the up, first pane view slides to the bottom.</li>
+                <li>In split vertical, when dragging to the left, the first pane view slides to the right.</li>
+                <li>In split horizontal, when dragging to the top, the first pane view slides to the bottom.</li>
                 <li>When dragging, objects inside the panes are being selected.</li>
                 <li>When dragging, the mousedown event sometimes triggers the sidebar action and slides the content.</li>
             </ul>
