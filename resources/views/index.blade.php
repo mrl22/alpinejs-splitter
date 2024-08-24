298cf2c5588fc7419b1e6f75fb8e4d31706a547f
Richard Leishman <richard@webfwd.co.uk>
2024-08-24 18:23:28 +0100

    Added Extra Context

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,16 @@
     <div class="p-10">
         <h1 class="text-2xl font-semibold mb-4">TailwindCSS/AlpineJS Laravel Splitter Component</h1>
 
-        <p><a class="underline text-blue-700" href="{{ route('split-v') }}">Split Vertical</a></p>
-        <p><a class="underline text-blue-700" href="{{ route('split-h') }}">Split Horizontal</a></p>
-        <p><a class="underline text-blue-700" href="{{ route('nested') }}">Nested</a></p>
-        <p><a class="underline text-blue-700" href="{{ route('fill-container') }}">Fill Container</a></p>
+        <p><a class="underline text-blue-700" href="{{ route('split-v') }}">Split Vertical</a> - <a target="_blank" class="underline text-gray-500" href="https://github.com/mrl22/alpinejs-splitter/blob/main/resources/views/split-v.blade.php">/resources/views/split-v.blade.php</a></p>
+        <p><a class="underline text-blue-700" href="{{ route('split-h') }}">Split Horizontal</a> - <a target="_blank" class="underline text-gray-500" href="https://github.com/mrl22/alpinejs-splitter/blob/main/resources/views/split-h.blade.php">/resources/views/split-h.blade.php</a></p>
+        <p><a class="underline text-blue-700" href="{{ route('nested') }}">Nested</a> - <a target="_blank" class="underline text-gray-500" href="https://github.com/mrl22/alpinejs-splitter/blob/main/resources/views/nested.blade.php">/resources/views/nested.blade.php</a></p>
+        <p><a class="underline text-blue-700" href="{{ route('fill-container') }}">Fill Container</a> - <a target="_blank" class="underline text-gray-500" href="https://github.com/mrl22/alpinejs-splitter/blob/main/resources/views/fill-container.blade.php">/resources/views/fill-container.blade.php</a></p>
 
+
+        <h1 class="text-2xl font-semibold mt-10 mb-4">Components</h1>
+
+        <p>Splitter Horizontal Component - <a target="_blank" class="underline text-gray-500" href="https://github.com/mrl22/alpinejs-splitter/blob/main/resources/views/components/splitter-h.blade.php">/resources/views/components/splitter-h.blade.php</a></p>
+        <p>Splitter Vertical Component - <a target="_blank" class="underline text-gray-500" href="https://github.com/mrl22/alpinejs-splitter/blob/main/resources/views/components/splitter-v.blade.php">/resources/views/components/splitter-v.blade.php</a></p>
 
         <div class="mt-10">
             <h1 class="text-2xl font-semibold mb-4">Known Issues</h1>
